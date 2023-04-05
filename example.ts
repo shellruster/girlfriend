f034073f5223afa662a20a644a2ef014de835849
Cybeast Falzar <phoenixbboy@gmail.com>
2023-04-05 17:22:22 +0500

    adding more examples

--- example.ts
+++ example.ts
@@ -4,8 +4,12 @@ interface Foo {
   bar: string;
   fizz: number;
 }
+
 let content: string;
+
+// @ts-ignore
 content = await girlfriend.fetch(
   "https://deno.land/std@0.177.0/examples/welcome.ts",
 );
+
 console.log("Content from fetch", content);
